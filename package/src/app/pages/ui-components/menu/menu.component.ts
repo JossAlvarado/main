4189c509b5753f3bb2db39fd8892078bf359e5c2
JossAlvardao <josseline.alvaradovargas@utxicotepec.edu.mx>
2023-12-01 14:23:23 -0600

    p8o7

--- package/src/app/pages/ui-components/menu/menu.component.ts
+++ package/src/app/pages/ui-components/menu/menu.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-tu-componente',
+  templateUrl: './menu.component.html',
+})
+export class TuComponenteComponent {
+  ventaForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.ventaForm = this.fb.group({
+      modelo: ['', Validators.required],
+      numeroZapatos: ['', [Validators.required, Validators.min(1)]],
+    });
+  }
+
+  registrarVenta() {
+    if (this.ventaForm.valid) {
+      // Puedes agregar la lógica para manejar la venta aquí
+      const formData = this.ventaForm.value;
+      console.log('Datos de venta:', formData);
+    } else {
+      // Puedes agregar lógica para manejar formularios inválidos si es necesario
+    }
+  }
+}
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html'
 })
 export class AppMenuComponent {
+  ventaForm: FormGroup; // Declara la variable del formulario
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    // Inicializa el formulario en el constructor
+    this.ventaForm = this.fb.group({
+      modelo: ['', Validators.required],
+      numeroZapatos: ['', [Validators.required, Validators.min(1)]],
+    });
+  }
 
-
+  registrarVenta() {
+    // Agrega la lógica que necesites al registrar la venta
+    const formData = this.ventaForm.value;
+    console.log('Datos de venta:', formData);
+  }
 }
