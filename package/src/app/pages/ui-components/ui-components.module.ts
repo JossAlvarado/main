2480a915d4b06d72ebe710430928c62856748281
JossAlvarado <josseline.alvaradovargas@utxicotepec.edu.mx>
2023-12-05 18:03:09 -0600

    jutru

--- package/src/app/pages/ui-components/ui-components.module.ts
+++ package/src/app/pages/ui-components/ui-components.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from '../../material.module';
+import { RouterModule } from '@angular/router';
+import { MatNativeDateModule } from '@angular/material/core';
 
-// icons
+import { MaterialModule } from '../../material.module';
 import { TablerIconsModule } from 'angular-tabler-icons';
 import * as TablerIcons from 'angular-tabler-icons/icons';
 
 import { UiComponentsRoutes } from './ui-components.routing';
 
-// ui components
+// UI components
 import { AppBadgeComponent } from './badge/badge.component';
 import { AppChipsComponent } from './chips/chips.component';
 import { AppListsComponent } from './lists/lists.component';
-import { AppMenuComponent } from './menu/menu.component';
 import { AppTooltipsComponent } from './tooltips/tooltips.component';
-import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   imports: [
@@ -32,8 +30,8 @@ import { MatNativeDateModule } from '@angular/material/core';
     AppBadgeComponent,
     AppChipsComponent,
     AppListsComponent,
-    AppMenuComponent,
     AppTooltipsComponent,
   ],
 })
 export class UicomponentsModule {}
+
